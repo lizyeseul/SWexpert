4052a7640b6873c44bdf62d632820b593938e999
ys <lys984prms@naver.com>
2020-01-04 14:31:34 +0900

    1926 정답

--- src/for_1926.java
+++ src/for_1926.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class for_1926 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+
+		for(int i = 1; i <= n; i++)
+		{
+			int clap = 0;
+			String num = Integer.toString(i);
+			for(int k=0; k<num.length(); k++) {
+				char c = num.charAt(k);
+				if(c == '3' || c == '6' || c == '9') {
+					clap++;
+				}
+			}
+			if(clap == 0) {
+				System.out.print(i+" ");
+			}
+			else {
+				for(int j=0; j<clap; j++) {
+					System.out.print("-");
+				}
+				System.out.print(" ");
+			}
+		}
 	}
-
 }
