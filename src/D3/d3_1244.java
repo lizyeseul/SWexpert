7c05a707fb910486d2e3bbe17da2d37b5f7e9a38
ys <lys984prms@naver.com>
2020-01-10 17:54:21 +0900

    1244 정답
    예외처리 많은 문제
    한시간 반 걸린듯

--- src/D3/d3_1244.java
+++ src/D3/d3_1244.java
@@ -16,47 +16,87 @@ public class d3_1244 {
 			int swapCount = sc.nextInt();
 			
 			//calculate
-			for(int i=0; i<swapCount; i++) {
-				int[] findM = max(numbers);
+			int sort = 0;
+			while(swapCount>0) {
+				if(isSort(numbers) == true) {
+					break;
+				}
+				int[] findM = max(numbers, sort);
 				int maxIndex = findM[1];
 				int count = findM[2];
-				int minIndex = minindex(numbers, count);
-				
+				int minIndex = minindex(numbers, count, sort);
+				while(minIndex == maxIndex) {
+					sort++;
+					findM = max(numbers, sort);
+					maxIndex = findM[1];
+					count = findM[2];
+					minIndex = minindex(numbers, count, sort);
+				}
+				if(minIndex == sort)	sort++;
+				System.out.println("max: "+maxIndex+", min: "+minIndex+", count: "+count);
+
+				System.out.print(numbers+"->");
 				char temp = numbers.charAt(maxIndex);
-				numbers = 
+				numbers = numbers.substring(0,maxIndex)+numbers.charAt(minIndex)+numbers.substring(maxIndex+1);
+				numbers = numbers.substring(0,minIndex)+temp+numbers.substring(minIndex+1);
+				System.out.println(numbers);
+				swapCount--;
+			}
+			if(swapCount != 0) {
+				if(swapCount %2==1) {
+					if(isSame(numbers) == false) {
+						//System.out.print(numbers+"->");
+						char temp = numbers.charAt(numbers.length()-1);
+						numbers = numbers.substring(0,numbers.length()-1)+numbers.charAt(numbers.length()-2)+numbers.substring(numbers.length());
+						numbers = numbers.substring(0,numbers.length()-2)+temp+numbers.substring(numbers.length()-1);
+						//System.out.println(numbers);
+					}
+				}
 			}
 			
 			//output
 			System.out.print("#"+test_case);
-			System.out.println(" "+result);
+			System.out.println(" "+numbers);
 		}
 	}
-	public static int[] max(String a) {
+	public static int[] max(String a, int sort) {
 		int[] result = {0,0,1};
-		for(int k=a.length(); k>=0; k--) {
-			if(a.charAt(k)>result[0]) {
+		for(int k=a.length()-1; k>=sort; k--) {
+			if(a.charAt(k)>(int)result[0]) {
 				result[0] = a.charAt(k);
 				result[1] = k;
 				result[2] = 1;
-			} else if(a.charAt(k)==result[0]) {
+			} else if(a.charAt(k)==(int)result[0]) {
 				result[2]++;
 			}
 		}
 		return result;
 	}
-	public static int minindex(String a, int count) {
-		int result=0;
-		for(int k=1; k>=a.length(); k++) {
+	public static int minindex(String a, int count, int i) {
+		int result=i;
+		for(int k=i+1; k<a.length(); k++) {
 			count--;
 			if(count==0) {
 				return result;
 			}
-			
 			if(a.charAt(k)<a.charAt(result)) {
 				result = k;
 			}
-			
 		}
 		return result;
+	}
+	public static boolean isSort(String a) {
+		for(int l=a.length()-1; l>0; l--) {
+			if(a.charAt(l-1) < a.charAt(l))
+				return false;
+		}
+		return true;
+	}
+	public static boolean isSame(String a) {
+		for(int m=0; m<a.length()-1; m++) {
+			if(a.charAt(m) == a.charAt(m+1))
+				return true;
+		}
+		return false;
 	}
 }
