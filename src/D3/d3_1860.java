fc3e4ce9d037d4afaf96cd628d299591032372b1
ys <lys984prms@naver.com>
2020-01-22 16:33:29 +0900

    1860 푸는 중
    mergesort 구현 중

--- src/D3/d3_1860.java
+++ src/D3/d3_1860.java
@@ -7,10 +7,26 @@ import java.util.StringTokenizer;
 
 public class d3_1860 {
 	public static void main(String[] args) throws IOException {
+		
+		
+
+		System.out.println("start");
+		int arr[] = {3,1,2,5,4};
+		arr = partition(arr, 0, 4);
+		
+		for(int k=0; k<5; k++) {
+			System.out.print(arr[k]+" ");
+		}
+		
+		
+		System.out.println("end");
+		
+		
+		/*
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=null;
-		int T;
-		T=br.read();
+		int T=1;
+		//T=br.read();
 		
 		for(int test_case = 1; test_case <= T; test_case++) {
 			int result=0;
@@ -41,6 +57,59 @@ public class d3_1860 {
             else {
                 System.out.println("#"+test_case+" Impossible");
             }
+		}*/
+	}
+	public static int[] partition(int[] arr, int left, int right) {
+		if(left>=right) {
+			return arr;
 		}
+		int middle = (right+left)/2;
+		arr = partition(arr, left, middle);
+		arr = partition(arr, middle+1, right);
+		arr = MergeSort(arr, left, middle, right);
+		
+
+		for(int k=0; k<5; k++) {
+			System.out.print(arr[k]+" ");
+		}
+		System.out.println();
+		
+		return arr;
+	}
+	public static int[] MergeSort(int[] arr, int left, int middle, int right) {
+		int[] temp = new int[right-left+1];
+	
+		int lIndex=left, rIndex=middle+1;
+		for(int i=left; i<=right; i++) {
+			//1 2 4   3 5
+			boolean max = (arr[lIndex]<arr[rIndex])?true:false;
+			if(max == true) {//left
+				int count=0;
+				while(arr[lIndex]<arr[rIndex] && (lIndex+count)<middle) {
+					count++;
+				}
+				for(int j=i; j<i+count; j++) {
+					temp[j] = arr[lIndex];
+					lIndex++;
+				}
+				i += count;
+			} else  {//left
+				int count=0;
+				while(arr[rIndex]<arr[lIndex] && (rIndex+count)<right) {
+					count++;
+				}
+				for(int j=i; j<i+count; j++) {
+					temp[j] = arr[rIndex];
+					rIndex++;
+				}
+				i += count;
+			}
+		}
+		
+		for(int i=left; i<=right; i++) {
+			arr[i] = temp[i-left];
+		}
+		
+		return arr;
 	}
 }
