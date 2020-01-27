8604fd20c3bdbef1c063d41d241d49c4799a56e5
ys <lys984prms@naver.com>
2020-01-27 11:09:50 +0900

    2819 푸는 중
    HashSet 이용 - 중복 알아서 걸러줌
    문제 조건 다 읽자
    뭐 틀린건지 모르겠음

---
+++ src/D4/d4_2819.java
@@ -0,0 +1,57 @@
+package D4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class d4_2819 {
+	static char[][] arr = new char[4][4];
+	static HashSet<String> map = new HashSet<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ys0star\\eclipse-workspace\\SWexpert\\src/input.txt"));
+//        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=null;
+		int T=Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+        	for(int i=0; i<4; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(int j=0; j<4; j++) {
+            		arr[i][j] = st.nextToken().charAt(0);
+        		}
+        	}
+        	for(int i=0; i<4; i++) {
+        		for(int j=0; j<4; j++) {
+                	count("", i,j);
+        		}
+        	}
+            System.out.println("#"+test_case+" "+map.size());
+		}
+	}
+	public static void count(String line, int x, int y) {
+		if(line.length() == 7) {
+			map.add(line);
+			return;
+		}
+		//System.out.println(x+" x : y "+y);
+		
+		line += arr[x][y];
+		
+		if(x>0)
+			count(line, x-1, y);
+		if(y>0)
+			count(line, x, y-1);
+		if(y<3)
+			count(line, x, y+1);
+		if(x<3)
+			count(line, x+1, y);
+			
+		line = line.substring(0, line.length()-1);
+		
+		return;
+	}
+}
